35bead65f63051062b8f98d36422f2a6fe605531
Sandeep Kumar <sandeepkumar@Sandeeps-MacBook-Air.local>
2025-05-20 22:15:28 +0530

    add logging

--- backend/movie-service/service/src/main/java/com/movie/booking/movie_service/model/request/CastRequest.java
+++ backend/movie-service/service/src/main/java/com/movie/booking/movie_service/model/request/CastRequest.java
@@ -1,14 +1,10 @@
 package com.movie.booking.movie_service.model.request;
 
 import com.movie.booking.movie_service.enums.CastRole;
-import lombok.Value;
 
-@Value
-public class CastRequest {
-    Long id; // Optional: present if updating an existing cast member
-    String name;
-    String imageUrl;
-    String characterName;
-    CastRole role;
-    String description;
+/**
+ * @param id Optional: present if updating an existing cast member
+ */
+public record CastRequest(Long id, String name, String imageUrl, String characterName, CastRole role,
+                          String description) {
 }
